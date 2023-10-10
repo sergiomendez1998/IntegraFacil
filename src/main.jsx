9ad2aa4d4e2cf03260bc922150996083e39f11f8
sergio-mendez-telus <sergio.mendezgonzalez@telus.com>
2023-10-09 21:33:59 -0600

    fixing

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import '../public/styles/WelcomePage.css'
-import '../public/styles/canvas.css'
+import './styles/WelcomePage.css'
+import './styles/canvas.css'
 import {IntegralesApp} from "./IntegralesApp";
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <IntegralesApp/>
-    </React.StrictMode>,
+    </React.StrictMode>
 )
